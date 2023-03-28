5c1ecb8225545abf71780218a0e4305dea5d17be
benjaminellmer <benjamin.ellmer@yahoo.com>
2023-03-28 15:05:50 +0200

    Add Colors

--- hookah_with_friends/lib/hookah_with_friends_app.dart
+++ hookah_with_friends/lib/hookah_with_friends_app.dart
@@ -2,6 +2,7 @@ import "package:flutter/material.dart";
 import "package:flutter_bloc/flutter_bloc.dart";
 
 import "bloc/navigation/navigation_cubit.dart";
+import "util/colors.dart";
 
 class HookahWithFriendsApp extends StatelessWidget {
   const HookahWithFriendsApp({super.key});
@@ -20,8 +21,11 @@ class HookahWithFriendsApp extends StatelessWidget {
                 title: const Text("Hookah with friends"),
                 backgroundColor: Colors.black54,
               ),
-              body: const Center(
-                child: Text("Welcome to Hookah with friends"),
+              body: Column(
+                children: <Widget>[
+                  const Center(child: Text("Welcome to Hookah with friends")),
+                  Container(width: 100, height: 50, color: HWFColors.button)
+                ],
               ),
               bottomNavigationBar: BottomNavigationBar(
                 type: BottomNavigationBarType.fixed,
---
+++ hookah_with_friends/lib/util/colors.dart
@@ -0,0 +1,14 @@
+// ignore_for_file: avoid_classes_with_only_static_members
+
+import "dart:ui";
+
+class HWFColors {
+  static Color button = const Color(0x0028afb0).withOpacity(0.8);
+  static Color text = const Color(0x00b9f8f4).withOpacity(0.8);
+  static Color heading = const Color(0x00ffb94f).withOpacity(0.8);
+  static Color subheading = const Color(0x00fde07a).withOpacity(0.8);
+  static Color divider = const Color(0x00ffdb80).withOpacity(0.3);
+  static Color green = const Color(0x0000ab07).withOpacity(0.8);
+  static Color orange = const Color(0x00ff9900).withOpacity(0.8);
+  static Color red = const Color(0x00ab0000).withOpacity(0.8);
+}
