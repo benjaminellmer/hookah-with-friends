91f07216a6059abda73ca624ed6ee53f5e288db3
benjaminellmer <benjamin.ellmer@yahoo.com>
2023-03-29 16:26:47 +0200

    Move screen logic into screens

--- hookah_with_friends/lib/hookah_with_friends_app.dart
+++ hookah_with_friends/lib/hookah_with_friends_app.dart
@@ -2,6 +2,9 @@ import "package:flutter/material.dart";
 import "package:google_fonts/google_fonts.dart";
 import "package:hookah_with_friends/util/colors.dart";
 
+import "screens/profile_screen.dart";
+import "screens/sessions_screen.dart";
+import "screens/tobaccos_screen.dart";
 import "util/hwf_screen.dart";
 import "util/hwf_tab_navigation.dart";
 
@@ -22,32 +25,24 @@ class HookahWithFriendsApp extends StatelessWidget {
       home: HWFTabNavigation(
         screens: <HWFScreen>[
           HWFScreen(
-            appBar: AppBar(
-              title: const Text("Sessions"),
-              backgroundColor: HWFColors.appbar,
-            ),
-            body: const Center(
-              child: Text("Sessions"),
-            ),
-          ),
+              appBar: AppBar(
+                title: const Text("Sessions"),
+                backgroundColor: HWFColors.appbar,
+              ),
+              body: const SessionsScreen()),
           HWFScreen(
             appBar: AppBar(
               title: const Text("Tobaccos"),
               backgroundColor: HWFColors.appbar,
             ),
-            body: const Center(
-              child: Text("Tobaccos"),
-            ),
+            body: const TobaccosScreen(),
           ),
           HWFScreen(
-            appBar: AppBar(
-              title: const Text("Profile"),
-              backgroundColor: HWFColors.appbar,
-            ),
-            body: const Center(
-              child: Text("Profile"),
-            ),
-          ),
+              appBar: AppBar(
+                title: const Text("Profile"),
+                backgroundColor: HWFColors.appbar,
+              ),
+              body: const ProfileScreen()),
         ],
       ),
     );
