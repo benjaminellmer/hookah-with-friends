efaf214e4258b5240676ebd993598a4e0cfb66dd
benjaminellmer <benjamin.ellmer@yahoo.com>
2023-03-29 17:52:42 +0200

    Implement Heading Text

---
+++ hookah_with_friends/lib/components/heading_text.dart
@@ -0,0 +1,14 @@
+import "package:flutter/cupertino.dart";
+import "package:google_fonts/google_fonts.dart";
+import "../util/colors.dart";
+
+class HeadingText extends Text {
+  HeadingText(super.data, {super.key})
+      : super(
+          style: TextStyle(
+            fontFamily: GoogleFonts.pacifico().fontFamily,
+            color: HWFColors.heading,
+            fontSize: 24,
+          ),
+        );
+}
