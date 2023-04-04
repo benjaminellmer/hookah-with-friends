5e219ad80c7cac8582d16da450523b2e718a87c6
benjaminellmer <benjamin.ellmer@yahoo.com>
2023-04-04 16:31:57 +0200

    first draft of sessions screen

--- hookah_with_friends/lib/screens/sessions_screen.dart
+++ hookah_with_friends/lib/screens/sessions_screen.dart
@@ -1,12 +1,33 @@
 import "package:flutter/material.dart";
 
+import "../components/primary_card.dart";
+import "../components/subheading.dart";
+
 class SessionsScreen extends StatelessWidget {
   const SessionsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Sessions"),
+    return Padding(
+      padding: const EdgeInsets.all(20),
+      child: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            const SubHeading("Active Sessions"),
+            PrimaryCard(
+              child: Column(
+                children: const <Widget>[
+                  Text("Host"),
+                  Text("Tobacco"),
+                  Text("Participants"),
+                ],
+              ),
+            ),
+            const SubHeading("Invites"),
+            const SubHeading("History"),
+          ],
+        ),
+      ),
     );
   }
 }
