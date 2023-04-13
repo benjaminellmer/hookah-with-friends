3433a3a88ba5974463ec803169fc35620c2be3fa
smayr <mayr97.sm@gmx.at>
2023-04-13 17:14:35 +0200

    Welcome Text Component created

---
+++ hookah_with_friends/lib/components/texts/welcome_text.dart
@@ -0,0 +1,14 @@
+import "package:flutter/cupertino.dart";
+import "package:google_fonts/google_fonts.dart";
+import '../../util/colors.dart';
+
+class WelcomeText extends Text {
+  WelcomeText(super.data, {super.key})
+      : super(
+          style: TextStyle(
+            fontFamily: GoogleFonts.pacifico().fontFamily,
+            color: HWFColors.heading.withOpacity(1),
+            fontSize: 56,
+          ),textAlign: TextAlign.center
+        );
+}
