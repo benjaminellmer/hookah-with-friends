7a7d3fa8d39a0c95117f0c02f4a1c0de6c8ed9ea
smayr <mayr97.sm@gmx.at>
2023-04-13 16:21:35 +0200

    replaced code with components

--- hookah_with_friends/lib/screens/login_screen.dart
+++ hookah_with_friends/lib/screens/login_screen.dart
@@ -1,15 +1,12 @@
 import "dart:ui";
 
 import "package:flutter/material.dart";
-import "package:flutter_bloc/flutter_bloc.dart";
-
-import "../bloc/coaltimer/coal_timer_bloc.dart";
 import "../components/buttons/primary_button.dart";
-import "../components/cards/tobacco_card.dart";
-import "../components/texts/heading_text.dart";
-import "../components/texts/primary_text.dart";
-import "../components/texts/subheading.dart";
-import "../model/session.dart";
+import "../components/buttons/secondary_button.dart";
+import "../components/textinputs/password_input.dart";
+import "../components/textinputs/username_input.dart";
+import "../components/texts/divider_text.dart";
+import "../components/texts/heading_text_XL.dart";
 import "../util/colors.dart";
 
 class LoginScreen extends StatelessWidget {
@@ -23,114 +20,71 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SingleChildScrollView(
-          child: Stack(
-            children: [
-              Container(
-                child: RotationTransition(
-                turns: new AlwaysStoppedAnimation(180/360),
+      body: SingleChildScrollView(
+        child: Stack(
+          children: [
+            Container(
+              child: RotationTransition(
+                turns: new AlwaysStoppedAnimation(180 / 360),
                 child: Image.network(
                   'https://images.unsplash.com/photo-1627499949691-154d1fc004c2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80',
                   width: MediaQuery.of(context).size.width,
                   height: MediaQuery.of(context).size.height,
-                  fit: BoxFit.cover,                   
-                ),
+                  fit: BoxFit.cover,
                 ),
               ),
-              Container(
-            padding: const EdgeInsets.all(20),
-            child: Column(
-              children: [
-                SizedBox(height: MediaQuery.of(context).size.height * 0.25),
-                Container(
-                  alignment: Alignment.centerLeft,
-                  child: HeadingText("Welcome \nBack!"),
-                ),
-                const SizedBox(height: 32),
-                TextFormField(
-                    controller: usernameController,
-                    decoration: const InputDecoration(
-                      focusedBorder: UnderlineInputBorder(
-                          borderSide: BorderSide.none,
-                          borderRadius:
-                          BorderRadius.all(Radius.circular(15))),
-                      enabledBorder: UnderlineInputBorder(
-                          borderSide: BorderSide.none,
-                          borderRadius:
-                          BorderRadius.all(Radius.circular(10))),
-                      prefixIcon: Icon(
-                        Icons.person,
-                        color: HWFColors.flavour,
-                      ),
-                      filled: true,
-                      fillColor: HWFColors.cardBackground,
-                      labelText: "Username",
-                      hintText: 'KopfalNorbert',
-                      labelStyle: TextStyle(color: HWFColors.flavour),
-                    ),
-                ),
-                SizedBox(height: 16),
-                Form(
-                  key: _formKey,
-                  child: TextFormField(
-                    obscureText: true,
-                    decoration: const InputDecoration(
-                      focusedBorder: UnderlineInputBorder(
-                          borderSide: BorderSide.none,
-                          borderRadius:
-                          BorderRadius.all(Radius.circular(10))),
-                      enabledBorder: UnderlineInputBorder(
-                          borderSide: BorderSide.none,
-                          borderRadius:
-                          BorderRadius.all(Radius.circular(10))),
-                      prefixIcon: Icon(
-                        Icons.lock_open,
-                        color: HWFColors.flavour,
-                      ),
-                      filled: true,
-                      fillColor: HWFColors.cardBackground,
-                      labelText: "Password",
-                      hintText: 'Password',
-                      labelStyle: TextStyle(color: HWFColors.flavour),
-                    ),
-                    validator: (value) {
-                      if (value!.isEmpty && value!.length < 5) {
-                        return 'Enter a valid password';
-                        {
-                          return null;
-                        }
-                      }
-                    },
+            ),
+            Container(
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                children: [
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.25),
+                  Container(
+                    alignment: Alignment.centerLeft,
+                    child: HeadingTextXL("Welcome \nBack!"),
                   ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    TextButton(
-                      onPressed: () {},
-                      child: Text('Forgot password?',
+                  const SizedBox(height: 32),
+                  UsernameInput(
+                      labelText: 'Username',
+                      hintText: 'KopfalNorberts',
+                      icon: Icons.person,
+                      controller: usernameController),
+                  SizedBox(height: 16),
+                  Form(
+                    key: _formKey,
+                    child: PasswordInput(
+                        labelText: 'Passwords',
+                        hintText: 'Password',
+                        icon: Icons.lock_open,
+                        controller: passwordController),
+                  ),
+                  Container(
+                    alignment: Alignment.centerRight,
+                    child: TextButton(
+                      onPressed: () {print('Forogot Password');},
+                      child: Text(
+                        'Forgot password?',
                         style: TextStyle(
                             color: HWFColors.flavour,
                             fontWeight: FontWeight.bold),
                       ),
-                    )
-                  ],
-                ),
-                PrimaryButton(
-                  text: "Log in",
-                  onPress: () {},
-                ),
-                SubHeading("or"),
-                PrimaryButton(
-                  text: "Sign up",
-                  onPress: () {},
-                ),
-              ],
+                    ),
+                  ),
+                  PrimaryButton(
+                    text: "Log in",
+                    onPress: () {print('Login');},
+                  ),
+                  DividerText("or"),
+                  SecondaryButton(
+                    text: "Sign up",
+                    onPress: () {print('Sign up');},
+                  ),
+                ],
+              ),
             ),
-          ),
-      ],
+          ],
         ),
-        ),
+      ),
     );
   }
 }
