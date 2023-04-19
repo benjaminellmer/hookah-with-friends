3f575f508aee45cfc656d3d75a21c631e3c5184b
DavidReiter <david.reiter4626@gmail.com>
2023-04-19 16:14:37 +0200

    Add edit icon and 'add' button in friends screen

--- hookah_with_friends/lib/components/appbars/back_and_title_app_bar.dart
+++ hookah_with_friends/lib/components/appbars/back_and_title_app_bar.dart
@@ -4,7 +4,7 @@ import '../../util/colors.dart';
 import '../texts/heading_text.dart';
 
 class BackAndTitleAppBar extends AppBar {
-  BackAndTitleAppBar({super.key, required final String title})
+  BackAndTitleAppBar({super.key, super.actions, required final String title})
       : super(
           centerTitle: true,
           title: HeadingText(title),
--- hookah_with_friends/lib/screens/friends_screen.dart
+++ hookah_with_friends/lib/screens/friends_screen.dart
@@ -2,8 +2,10 @@ import "package:flutter/cupertino.dart";
 import "package:flutter/material.dart";
 
 import "../components/appbars/back_and_title_app_bar.dart";
+import "../components/buttons/primary_button.dart";
 import "../components/cards/friend_card.dart";
 import "../model/friend.dart";
+import "../util/colors.dart";
 import "../util/testdata/testdata.dart";
 
 class FriendsScreen extends StatelessWidget {
@@ -12,18 +14,43 @@ class FriendsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: BackAndTitleAppBar(title: "Friends"),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Column(
-            children: <Widget>[
-              const SizedBox(height: 10),
-              for (Friend friend in TestData.friendList) ...<FriendCard>[
-                FriendCard(friend: friend)
-              ]
-            ],
+      appBar: BackAndTitleAppBar(
+        title: "Friends",
+        actions: [
+          IconButton(
+            onPressed: () {
+              
+            },
+            icon: const Icon(Icons.edit),
+            color: HWFColors.heading,
           ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column (
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            const SizedBox(height: 10),
+            Expanded (
+              child: SingleChildScrollView(
+                child: Column(
+                  children: <Widget>[
+                    for (Friend friend in TestData.friendList) ...<FriendCard>[
+                      FriendCard(friend: friend)
+                    ]
+                  ],
+                )
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            PrimaryButton(
+              text: "Add Friend",
+              onPress: () {
+
+              },
+            ),
+          ],
         ),
       ),
     );
