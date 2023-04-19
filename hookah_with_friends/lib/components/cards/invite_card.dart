ca42864755720cf0b1bc4ec7cdff7c588f989f13
DavidReiter <david.reiter4626@gmail.com>
2023-04-19 17:25:58 +0200

    Changed icons in invite_card to iconbuttons

--- hookah_with_friends/lib/components/cards/invite_card.dart
+++ hookah_with_friends/lib/components/cards/invite_card.dart
@@ -1,4 +1,5 @@
 import "package:flutter/cupertino.dart";
+import "package:flutter/material.dart";
 
 import "../../model/friend.dart";
 import "../texts/primary_text.dart";
@@ -19,18 +20,28 @@ class InviteCard extends StatelessWidget {
             fontSize: 16,
           ),
           const Spacer(),
-          Image.asset(
-            "lib/assets/cross.png",
-            width: 20,
-            height: 20,
+          IconButton(
+            constraints: const BoxConstraints(maxWidth: 20.0, maxHeight: 20.0),
+            padding: EdgeInsets.zero,
+            onPressed: () {},
+            icon: Image.asset(
+              "lib/assets/cross.png",
+              width: 20,
+              height: 20,
+            ),
           ),
           const SizedBox(
             width: 20,
           ),
-          Image.asset(
-            "lib/assets/checkmark.png",
-            width: 20,
-            height: 20,
+          IconButton(
+              constraints: const BoxConstraints(maxWidth: 20.0, maxHeight: 20.0),
+              padding: EdgeInsets.zero,
+              onPressed: () {},
+              icon: Image.asset(
+                "lib/assets/checkmark.png",
+                width: 20,
+                height: 20,
+              ),
           ),
         ],
       ),
