8e30a04937593f7da52d2d2986803747f98db1a0
Mayr08 <mayr97.sm@gmx.at>
2023-04-26 17:01:09 +0200

    added Google Button to Login Screen

--- hookah_with_friends/lib/screens/login_screen.dart
+++ hookah_with_friends/lib/screens/login_screen.dart
@@ -2,6 +2,7 @@ import "package:flutter/material.dart";
 import "package:flutter_bloc/flutter_bloc.dart";
 
 import "../bloc/auth/auth_bloc.dart";
+import "../components/buttons/google_button.dart";
 import "../components/buttons/primary_button.dart";
 import "../components/buttons/secondary_button.dart";
 import "../components/dialogs/forgot_password_dialog.dart";
@@ -108,6 +109,9 @@ class LoginScreen extends StatelessWidget {
                             );
                       },
                     ),
+                    GoogleButton(onPress: () {
+                      print("Login with google");
+                    }),
                     const DividerText("or"),
                     SecondaryButton(
                       text: "Sign up",
