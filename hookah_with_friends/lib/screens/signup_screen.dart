a91f64eea78c4b0f62f267edcd1bb0c81f04f0eb
JakodDittrich <73758485+JakodDittrich@users.noreply.github.com>
2023-04-26 17:20:28 +0200

    Update signup_screen.dart

--- hookah_with_friends/lib/screens/signup_screen.dart
+++ hookah_with_friends/lib/screens/signup_screen.dart
@@ -76,7 +76,7 @@ class SignupScreen extends StatelessWidget {
                     Form(
                       key: _formKey,
                       child: PasswordInput(
-                          labelText: "Passwords",
+                          labelText: "Password",
                           hintText: "Password",
                           icon: Icons.lock_open,
                           controller: passwordController),
