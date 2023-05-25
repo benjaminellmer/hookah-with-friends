aa3ad58cc1e6ab5877186d4cdf0b0b42823b650b
benjaminellmer <benjamin.ellmer@yahoo.com>
2023-05-25 22:36:22 +0200

    reloadable session screen

--- hookah_with_friends/lib/screens/profile_screen.dart
+++ hookah_with_friends/lib/screens/profile_screen.dart
@@ -1,9 +1,9 @@
 import "package:flutter/material.dart";
+import "package:flutter_bloc/flutter_bloc.dart";
 
+import "../bloc/profile/profile_cubit.dart";
 import "../components/cards/profile_info_card.dart";
 import "../components/cards/user_card.dart";
-import "../services/user_service.dart";
-import "../util/locator.dart";
 import "friends_screen.dart";
 import "invites_screen.dart";
 
@@ -12,37 +12,55 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Scaffold(
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(20),
-            child: Column(
-              children: <Widget>[
-                const UserCard(),
-                const SizedBox(height: 30),
-                ProfileInfoCard(
-                  info:
-                      "Friends (${getIt.get<UserService>().currentUser?.friends.length})",
-                  route: const FriendsScreen(),
+    return BlocProvider<ProfileCubit>(
+      create: (BuildContext context) => ProfileCubit()..loadUser(),
+      child: Builder(builder: (BuildContext context) {
+        return RefreshIndicator(
+          onRefresh: () async {
+            context.read<ProfileCubit>().loadUser();
+          },
+          child: Center(
+            child: Scaffold(
+              body: Padding(
+                padding: const EdgeInsets.all(20),
+                child: BlocBuilder<ProfileCubit, ProfileState>(
+                  builder: (BuildContext context, ProfileState state) {
+                    if (state is ProfileLoaded) {
+                      return ListView(
+                        children: <Widget>[
+                          const UserCard(),
+                          const SizedBox(height: 30),
+                          ProfileInfoCard(
+                            info:
+                                "Friends (${state.currentUser.friends.length})",
+                            route: const FriendsScreen(),
+                          ),
+                          ProfileInfoCard(
+                            info: "Invites (${state.numberOfInvitations})",
+                            route: const InvitesScreen(),
+                          ),
+                          // const ProfileInfoCard(
+                          //   info: "Sessions",
+                          //   route: HistoryScreen(),
+                          // ),
+                          // const ProfileInfoCard(
+                          //   info: "Settings",
+                          //   route: SettingsScreen(),
+                          // ),
+                        ],
+                      );
+                    } else {
+                      return const Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    }
+                  },
                 ),
-                const ProfileInfoCard(
-                  info: "Invites",
-                  route: InvitesScreen(),
-                ),
-                // const ProfileInfoCard(
-                //   info: "Sessions",
-                //   route: HistoryScreen(),
-                // ),
-                // const ProfileInfoCard(
-                //   info: "Settings",
-                //   route: SettingsScreen(),
-                // ),
-              ],
+              ),
             ),
           ),
-        ),
-      ),
+        );
+      }),
     );
   }
 }
